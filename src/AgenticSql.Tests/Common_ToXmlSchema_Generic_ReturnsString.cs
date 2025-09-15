888fd95fba726f44974e9fd3227912fa8670d0bc
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:03:31 -0600

    Add src/AgenticSql.Tests/Common_ToXmlSchema_Generic_ReturnsString.cs

---
+++ src/AgenticSql.Tests/Common_ToXmlSchema_Generic_ReturnsString.cs
@@ -0,0 +1,14 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgenticSql;
+
+[TestClass]
+public class Common_ToXmlSchema_Generic_ReturnsString
+{
+    [TestMethod]
+    public void ToXmlSchema_Generic_ReturnsNonEmpty()
+    {
+        string xml = InputXmlSchemas.EmptyXsd();
+        Assert.IsFalse(string.IsNullOrWhiteSpace(xml));
+    }
+}
