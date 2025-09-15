7ad7a545ef10e1dcf9f148918837a902a6f733d9
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:02:51 -0600

    Add src/AgenticSql.Tests/AgentFactory_CreateAgent_NullSqlStrings_ThrowsArgumentNull.cs

---
+++ src/AgenticSql.Tests/AgentFactory_CreateAgent_NullSqlStrings_ThrowsArgumentNull.cs
@@ -0,0 +1,17 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgenticSql;
+using System;
+
+namespace AgenticSql.Tests
+{
+    [TestClass]
+    public class AgentFactory_CreateAgent_NullSqlStrings_ThrowsArgumentNull
+    {
+        [TestMethod]
+        public void CreateAgent_NullSqlStrings_ThrowsArgumentNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => AgentFactory.CreateAgent((SqlStrings)null!, 3));
+        }
+    }
+}
