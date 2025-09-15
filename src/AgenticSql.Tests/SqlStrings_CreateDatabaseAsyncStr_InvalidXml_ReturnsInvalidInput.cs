8ec75d829bff0e1206fef5774c8843de20d8d20c
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:04:31 -0600

    Add src/AgenticSql.Tests/SqlStrings_CreateDatabaseAsyncStr_InvalidXml_ReturnsInvalidInput.cs

---
+++ src/AgenticSql.Tests/SqlStrings_CreateDatabaseAsyncStr_InvalidXml_ReturnsInvalidInput.cs
@@ -0,0 +1,16 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using AgenticSql;
+
+[TestClass]
+public class SqlStrings_CreateDatabaseAsyncStr_InvalidXml_ReturnsInvalidInput
+{
+    [TestMethod]
+    public async Task CreateDatabaseAsyncStr_InvalidXml()
+    {
+        var sql = new SqlStrings();
+        var res = await sql.CreateDatabaseAsyncStr("not xml");
+        Assert.IsTrue(res.Contains("Invalid input XML"));
+    }
+}
