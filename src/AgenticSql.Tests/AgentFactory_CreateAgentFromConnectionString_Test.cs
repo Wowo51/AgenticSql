29f5cb3313122020b7a97359691ae13db5f1ccbb
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:02:40 -0600

    Add src/AgenticSql.Tests/AgentFactory_CreateAgentFromConnectionString_Test.cs

---
+++ src/AgenticSql.Tests/AgentFactory_CreateAgentFromConnectionString_Test.cs
@@ -0,0 +1,17 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgenticSql.Tests
+{
+    [TestClass]
+    public sealed class AgentFactory_CreateAgentFromConnectionString_Test
+    {
+        [TestMethod]
+        public async Task CreateAgentFromConnectionString_localdb_master_returnsAgent()
+        {
+            string conn = "Server=(localdb)\\MSSQLLocalDB;Database=master;Integrated Security=True;TrustServerCertificate=True";
+            var agent = await AgenticSql.AgentFactory.CreateAgentFromConnectionStringAsync(conn, 1);
+            Assert.IsNotNull(agent);
+        }
+    }
+}
