d7aa6b0033071474ff86b32b9351af2632e35aff
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:03:07 -0600

    Add src/AgenticSql.Tests/Common_ExtractXml_ReturnsXml.cs

---
+++ src/AgenticSql.Tests/Common_ExtractXml_ReturnsXml.cs
@@ -0,0 +1,16 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgenticSql;
+
+[TestClass]
+public class Common_ExtractXml_ReturnsXml
+{
+    [TestMethod]
+    public void ExtractsSimpleXmlFromWrappedText()
+    {
+        string input = "prefix <?xml version=\"1.0\"?><Test>ok</Test> suffix";
+        var xml = Common.ExtractXml(input);
+        Assert.IsNotNull(xml);
+        Assert.IsTrue(xml.Contains("<Test>"));
+    }
+}
