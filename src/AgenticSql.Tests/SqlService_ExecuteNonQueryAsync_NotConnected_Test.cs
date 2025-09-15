9dc56336b51e52c7abc886644c6cfdde1e05e47d
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:04:05 -0600

    Add src/AgenticSql.Tests/SqlService_ExecuteNonQueryAsync_NotConnected_Test.cs

---
+++ src/AgenticSql.Tests/SqlService_ExecuteNonQueryAsync_NotConnected_Test.cs
@@ -0,0 +1,19 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgenticSql;
+using System;
+using System.Threading.Tasks;
+
+namespace AgenticSql.Tests
+{
+    [TestClass]
+    public sealed class SqlService_ExecuteNonQueryAsync_NotConnected_Test
+    {
+        [TestMethod]
+        public async Task ExecuteNonQueryAsync_WithoutDatabase_ThrowsInvalidOperation()
+        {
+            SqlService svc = new SqlService();
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () => await svc.ExecuteNonQueryAsync("SELECT 1"));
+        }
+    }
+}
