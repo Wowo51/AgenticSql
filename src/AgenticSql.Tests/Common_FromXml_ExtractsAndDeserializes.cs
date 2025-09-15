ec4d81e8b94b8bff40adf1fac7e8f700edeadaf2
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:03:20 -0600

    Add src/AgenticSql.Tests/Common_FromXml_ExtractsAndDeserializes.cs

---
+++ src/AgenticSql.Tests/Common_FromXml_ExtractsAndDeserializes.cs
@@ -0,0 +1,14 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgenticSql;
+
+[TestClass]
+public class Common_FromXml_ExtractsAndDeserializes
+{
+    [TestMethod]
+    public void FromXml_HandlesWrappedXml()
+    {
+        string xml = InputXmlSchemas.EmptyXsd();
+        Assert.IsFalse(string.IsNullOrEmpty(xml));
+    }
+}
