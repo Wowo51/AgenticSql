6a1e1178dc6da38eec45259c3ddc77dbfa6fef74
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:02:43 -0600

    Add src/AgenticSql.Tests/AgentFactory_CreateAgentWithDefaultServer_MethodTests.cs

---
+++ src/AgenticSql.Tests/AgentFactory_CreateAgentWithDefaultServer_MethodTests.cs
@@ -0,0 +1,19 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AgenticSql;
+
+namespace AgenticSql.Tests
+{
+    [TestClass]
+    public class AgentFactory_CreateAgentWithDefaultServer_MethodTests
+    {
+        [TestMethod]
+        public async Task ValidDbNameWithOverride_ReturnsSqlAgent()
+        {
+            var agent = await AgentFactory.CreateAgentWithDefaultServer("TestDb", 1, @"Server=(localdb)\\MSSQLLocalDB;Database=master;");
+            Assert.IsNotNull(agent);
+            Assert.IsInstanceOfType(agent, typeof(SqlAgent));
+        }
+    }
+}
