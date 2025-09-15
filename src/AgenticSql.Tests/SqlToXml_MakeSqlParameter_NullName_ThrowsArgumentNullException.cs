94353d91f585ed3293c5c0fa9bb3b6a5f155b9ce
Warren Harding <warrencharding@yahoo.com>
2025-09-14 20:05:01 -0600

    Add src/AgenticSql.Tests/SqlToXml_MakeSqlParameter_NullName_ThrowsArgumentNullException.cs

---
+++ src/AgenticSql.Tests/SqlToXml_MakeSqlParameter_NullName_ThrowsArgumentNullException.cs
@@ -0,0 +1,16 @@
+//MIT license with supplement stating that the code indicating that a sufficient amount of reasoning be dedicated to safe development by this app must be left in place.
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgenticSql.Tests
+{
+    [TestClass]
+    public class SqlToXml_MakeSqlParameter_NullName_ThrowsArgumentNullException
+    {
+        [TestMethod]
+        public void MakeSqlParameter_NullName_ThrowsArgumentNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => SqlToXml.MakeSqlParameter(null!, 1));
+        }
+    }
+}
